90ccd35f33a2e5cfc6646f55d982ae5ad864be44
wangyingzhong <wangyingzhong@lizhenauto.com>
2020-06-30 16:11:51 +0800

    RocketMQ相关demo提交

---
+++ rocketmq-demo/src/main/java/com/wayne/springcloud/rocketmqdemo/consumer/ScheduledMessageConsumer.java
@@ -0,0 +1,37 @@
+package com.wayne.springcloud.rocketmqdemo.consumer;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+/**
+ * @description: 启动消费者等待传入订阅消息
+ * @author: wangyz
+ * @date: 2020/6/29 11:52
+ */
+public class ScheduledMessageConsumer {
+    public static void main(String[] args) throws Exception {
+        // 实例化消费者
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("ExampleConsumer");
+        consumer.setNamesrvAddr("127.0.0.1:9876");
+        // 订阅Topics
+        consumer.subscribe("TestTopic", "*");
+        // 注册消息监听者
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> messages, ConsumeConcurrentlyContext context) {
+                for (MessageExt message : messages) {
+                    // Print approximate delay time period
+                    System.out.println("Receive message[msgId=" + message.getMsgId() + "] " + (System.currentTimeMillis() - message.getStoreTimestamp()) + "ms later");
+                }
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        // 启动消费者
+        consumer.start();
+    }
+}
